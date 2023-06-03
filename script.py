6fc9074f4f20e4902e99d37b8b6eb129f13b30be
Sehmon Burnam <sehmon3@gmail.com>
2023-06-03 14:58:48 +0800

    Created laptop/speaker test environments and a few docstrings

--- script.py
+++ script.py
@@ -24,43 +24,102 @@ load_dotenv()
 openai.api_key = os.environ['OPENAI_API_KEY'] # OpenAI
 set_api_key(os.environ["ELEVEN_LABS_API_KEY"]) # ElevenLabs
 
-# Define Constants
-DATA_TYPE = constants.DATA_TYPE
-
-# Define global variables
+# Define global variables and constants
 pool = concurrent.futures.ThreadPoolExecutor(max_workers=8) # Thread pool for playing audio on multiple sound devices
+DATA_TYPE = constants.DATA_TYPE
+SPEAKER_ENVIRONMENT = 'LAPTOP' # 'LAPTOP' | 'SCULPTURE'
 
 
 class SublimeSpeaker:
+
+  def __init__(self):
+    self.voices = voices()
+
   def run(self):
-    # Testing out playing audio on multiple sound devices
     while True:
-      output_streams = self.get_available_output_streams()
       relationship_pair = random.choice(constants.VOICEMAIL_PAIRS)
       voicemail_text = self.generate_voicemail(relationship_pair[0], relationship_pair[1])
-
-      import pdb; pdb.set_trace()
       
       # Generate audio, save, and play
-      audio = self.call_elevenlabs_api(voicemail_text, voices)
+      audio = self.call_elevenlabs_api(voicemail_text, self.voices)
 
       filename = "voicemail_{}_{}_{}.mp3".format(
       relationship_pair[0],
       relationship_pair[1],
       datetime.now().strftime("%Y%m%d_%H%M%S")
       )
-      # self.save_audio(audio, filename)
+
+      self.save_audio(audio, filename)
 
       # preamble_audio = self.generate_intro()
       # pool.submit(self.play_audio, preamble_audio)
-      # stream = self.create_running_output_stream(0)
-      # pool.submit(self.play_audio, filename)
-      # self.play_audio(filename, output_streams)
+
+      if SPEAKER_ENVIRONMENT == 'LAPTOP':
+        stream = self.create_running_output_stream(1)
+        pool.submit(self.play_audio, filename, [stream])
+      elif SPEAKER_ENVIRONMENT == 'SCULPTURE':
+        output_streams = self.get_available_output_streams()
+        self.play_audio(filename, output_streams)
 
       # Sleep 😴
-      n = 20
+      n = 1/30
       print("Waiting {} minute(s)...".format(n))
       time.sleep(n * 60)
+
+  def generate_voicemail(self, relationship_pair_a, relationship_pair_b):
+    """
+    Generate a voicemail prompt based on a conversatio between relationship_pair_a and relationship_pair_b.
+    :param relationship_pair_a: the voicemail caller
+    :param relationship_pair_b: the voicemail recipient
+    :return: a string containing the final voicemail text
+    """
+    voicemail_prompt = "Generate a random voicemail a {} would leave for a {}. Give both characters a name:".format(
+      relationship_pair_a,
+      relationship_pair_b
+      )
+    
+    print("Prompt to generate:")
+    print(voicemail_prompt)
+    prompt_response = self.call_openai_api(voicemail_prompt)
+    
+    print("Response:")
+    print(prompt_response)
+    return prompt_response
+
+
+  def call_openai_api(self, prompt):
+    """
+    Submit a prompt to the OpenAI completion API and return the response.
+    :param prompt: the text to submit to the API
+    :return: the text of the most likey response from the API
+    """
+    print("Calling OpenAI API...")
+    response = openai.Completion.create(
+      engine="text-davinci-003",
+      prompt=prompt,
+      max_tokens=240,
+      temperature=0.9,
+    )
+    return response.choices[0].text
+
+
+  def call_elevenlabs_api(self, prompt, voices):
+    """
+    Send a text string to the ElevenLabs API and return the response.
+    :param prompt: the text to submit to the API
+    :param voices: a list of possible voices to use for the response
+    :return response: the audio data returned from the API
+    """
+    print("Calling ElevenLabs API...")
+    print("Voice: {}".format(voices.voices))
+    v = random.choice(voices.voices)
+    response = None
+    try:
+      response = generate(text=prompt, voice=v)
+    except requests.exceptions.HTTPError as err:
+      print("Error: {0}".format(err))
+
+    return response
 
 
   def create_running_output_stream(self, index):
@@ -98,60 +157,24 @@ class SublimeSpeaker:
     if len(usb_sound_card_indicies) == 0:
       raise Exception("No USB sound cards found. Check you connections")
 
-    return [self.create_running_output_stream(index) for index in usb_sound_card_indicies]
-  
-
-  def generate_voicemail(self, relationship_pair_a, relationship_pair_b):
-    """
-    Generate a voicemail prompt based on a conversatio between relationship_pair_a and relationship_pair_b.
-    :param relationship_pair_a: the voicemail caller
-    :param relationship_pair_b: the voicemail recipient
-    :return: a string containing the final voicemail text
-    """
-    voicemail_prompt = "Generate a random voicemail a {} would leave for a {}. Give both characters a name:".format(
-      relationship_pair_a,
-      relationship_pair_b
-      )
-    
-    print("Prompt to generate:")
-    print(voicemail_prompt)
-    prompt_response = self.call_openai_api(voicemail_prompt)
-    
-    print("Response:")
-    print(prompt_response)
-    return prompt_response
-    
+    return [self.create_running_output_stream(index) for index in usb_sound_card_indicies]   
 
 
   def call_nytimes_api(self):
     response = requests.get("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=" + os.environ['NYTIMES_API_KEY'])
     response.raise_for_status()
     return response.json()
+  
 
-
-  def call_openai_api(self, prompt):
-    print("Calling OpenAI API...")
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=prompt,
-      max_tokens=240,
-      temperature=0.9,
-    )
-    return response.choices[0].text
-
-
-  def call_elevenlabs_api(self, prompt, voices):
-    print("Calling ElevenLabs API...")
-    print("Voice: {}".format(voices.voices))
-    v = random.choice(voices.voices)
-    response = None
-    try:
-      response = generate(text=prompt, voice=v)
-    except requests.exceptions.HTTPError as err:
-      print("Error: {0}".format(err))
-
-    return response
-
+  def generate_intro(self):
+    # preamble = "You have {} unheard messages. First unheard message:".format(math.floor(random.random() * 10))
+    preamble = "Next unheard message:"
+    tts_en = gTTS(preamble, lang='en')
+    audio_fp = BytesIO()
+    tts_en.write_to_fp(audio_fp)
+    audio_fp.seek(0)
+    return audio_fp.read()
+  
 
   def save_audio(self, audio, filename):
     print("Saving audio...")
@@ -177,16 +200,6 @@ class SublimeSpeaker:
     except Exception as e:
       print(e)
       print("Error writing to speaker {}".format(index))
-
-
-  def generate_intro(self):
-    # preamble = "You have {} unheard messages. First unheard message:".format(math.floor(random.random() * 10))
-    preamble = "Next unheard message:"
-    tts_en = gTTS(preamble, lang='en')
-    audio_fp = BytesIO()
-    tts_en.write_to_fp(audio_fp)
-    audio_fp.seek(0)
-    return audio_fp.read()
 
 
 if __name__ == '__main__':
